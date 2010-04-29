867b20e695e2b3770e150b0e844cdb6addd48ba4
Josh Bassett <josh.bassett@gmail.com>
2010-04-29 13:49:28 +1000

    Fixed render task.

--- lib/blammo/cli.rb
+++ lib/blammo/cli.rb
@@ -17,8 +17,7 @@ module Blammo
       commits  = Git.commits(repo_path, last_sha)
 
       unless commits.empty?
-        release  = Time.now.strftime("%Y%m%d%H%M%S")
-
+        release = Time.now.strftime("%Y%m%d%H%M%S")
         releases.unshift(release => commits)
         releases.to_yaml(open(changelog_path, "w"))
       end
@@ -26,27 +25,12 @@ module Blammo
       puts releases.to_yaml
     end
 
-    desc "render PATH", "Renders the given changelog.yml file"
-    def render(path)
+    desc "render [PATH]", "Renders the given changelog.yml file"
+    def render(path = "changelog.yml")
       releases = YAML.load_file(path)
-
-      releases.each do |release_hash|
-        release, groups = release_hash.first
-        puts release
-
-        groups.each do |group_hash|
-          group, commits = group_hash
-          puts "  " + group
-
-          commits.each do |commit_hash|
-            sha, message = commit_hash.first
-            puts "    " + message
-          end
-        end
-      end
-
-      template = Tilt.new(File.expand_path("../../templates/changelog.markdown.erb", __FILE__))
-      puts template.render(nil, :date => Date.today)
+      template_path = __FILE__.to_fancypath.dir / "../../templates/changelog.markdown.erb".to_fancypath
+      template = Tilt.new(template_path)
+      puts template.render(nil, :releases => releases)
     end
 
     def self.find_last_sha(releases)
