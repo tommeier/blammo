89a5239f12fe4a2cf510b5465eed1055434c11dd
Josh Bassett <josh.bassett@gmail.com>
2010-06-30 10:03:13 +1000

    Updated blank monkey patch comments.

--- lib/core_ext/blank.rb
+++ lib/core_ext/blank.rb
@@ -1,5 +1,5 @@
 class String
-  #Overwrites the command in Rails and causes odd behaviour
+  # Play nice with Rails.
   unless method_defined?(:blank?)
     def blank?
       empty?
@@ -8,7 +8,7 @@ class String
 end
 
 class NilClass
-  #Overwrites the command in Rails and causes odd behaviour
+  # Play nice with Rails.
   unless method_defined?(:blank?)
     def blank?
       true
