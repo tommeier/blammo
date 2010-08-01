61b9a6e2b3374be9638d9dfb05146d8ef5b10c86
Josh Bassett <josh.bassett@gmail.com>
2010-08-01 20:15:13 +1000

    Removed old bundler config.

--- lib/blammo/alone.rb
+++ lib/blammo/alone.rb
@@ -1,24 +1,14 @@
 rc = "#{ENV['HOME']}/.rubyrc"
 load(rc) if File.exist?(rc)
 
-ENV['BUNDLE_GEMFILE'] ||= File.join(File.expand_path(File.dirname(__FILE__)), '..', '..', 'Gemfile')
-
-begin
-  # Require the preresolved locked set of gems.
-  require File.expand_path('../../.bundle/environment', __FILE__)
-rescue LoadError
-  # Fallback on doing the resolve at runtime.
-  require 'rubygems'
-  require 'bundler'
-
-  Bundler.setup
-end
+require 'rubygems'
+require 'bundler'
 
 envs = [:default]
 envs << ENV['BLAMMO_ENV'].downcase.to_sym if ENV['BLAMMO_ENV']
-Bundler.require *envs
+Bundler.setup(*envs)
 
 path = File.join(File.expand_path(File.dirname(__FILE__)), '..')
-$:.unshift path
+$:.unshift(path)
 
 require 'blammo'
